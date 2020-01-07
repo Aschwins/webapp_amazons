875fdd0b7ac9e6e5ef61a0f926764031b583aac7
Aschwins <aschwin.schilperoort@gmail.com>
2020-01-07 18:29:50 +0100

    adding que to play a game

--- src/amazons/app.py
+++ src/amazons/app.py
@@ -23,8 +23,7 @@ async_mode = None
 
 socketio = SocketIO(app, async_mode=async_mode)
 
-clients = []
-clients_in_waiting_room = []
+clients_in_waiting = []
 game_number = 0
 
 
@@ -32,12 +31,11 @@ def create_game(sid1, sid2, game):
     """
     Function that pairs two players from the waiting room to a amazons game.
     """
-
-    join_room(game, sid1, '/test')
-    join_room(game, sid2, '/test')
+    join_room(str(game), sid1, '/test')
+    join_room(str(game), sid2, '/test')
     leave_room('waiting_room', sid1, '/test')
     leave_room('waiting_room', sid2, '/test')
-    emit('my_response', {'data': f"Created game #{game}, between {sid1} and {sid2}"}, broadcast=True)
+    emit('server_response', {'data': f"Created game #{str(game)}, between {sid1} and {sid2}"}, broadcast=True)
 
 
 @app.route("/", methods=["GET"])
@@ -46,61 +44,44 @@ def index():
     print(session)
     return render_template('index.html')
 
-
-@app.route("/hello/<name>", methods=["GET"])
-def get_hello(name):
-    return f"Hello {name}"
-
-
 @app.route("/game")
 def game():
     return render_template('game.html')
 
 
-# Sockets
-@socketio.on('my_event')
-def handle_message(message):
-    logger.info('recieved message: ' + message)
-    emit('my_response', 'This is my response')
-
-
 @socketio.on('connect', namespace='/test')
-def test_connect():
-    clients.append(request.sid)
-    logger.info(f"Client added: {clients}")
-    emit('my_response', {'data': f'Connected {request.sid}'}, broadcast=True)
+def connect():
+    emit('server_response', {'data': f'Connected {request.sid}'}, broadcast=True)
 
 
 @socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    clients.remove(request.sid)
-    clients_in_waiting_room.remove(request.sid)
-    emit('my_response', {'data': f'Disconnected {request.sid}'}, broadcast=True)
+def disconnect():
+    emit('server_response', {'data': f'Disconnected {request.sid}'}, broadcast=True)
     logger.info(f"Client disconnected: {request.sid}")
 
 
 @socketio.on('join', namespace='/test')
 def join(message):
-    join_room(message['room'])
-    logger.info(f"ROOMS: {rooms()}")
-    emit('my_response', {'data': 'In rooms: ' + ', '.join(rooms())}, broadcast=True)
-    emit('joined_waiting_room', request.sid)
-    clients_in_waiting_room.append(request.sid)
+    emit('server_response', {'data': f"Client {request.sid} joined waiting."}, broadcast=True)
+    clients_in_waiting.append(request.sid)
+    logger.info(f"CLIENTS IN WAITING: {clients_in_waiting}")
+    emit('update_waiting_room', {'clients_in_waiting': clients_in_waiting}, broadcast=True)
 
-    # Check if the waiting room contains enough players.
-    if len(clients_in_waiting_room) > 1:
-        # Create a new room where both clients are joined.
-        for client in clients_in_waiting_room:
-            if client != request.sid:
-                other_player = client
-                break
-
-        global game_number
-        game_number += 1
-        create_game(request.sid, other_player, game_number)
-        clients_in_waiting_room.remove(request.sid)
-        clients_in_waiting_room.remove(other_player)
-        emit('redirect', {'url': url_for('game')})
+    # # Check if the waiting room contains enough players.
+    # if len(clients_in_waiting) > 1:
+    #     # Create a new room where both clients are joined.
+    #     for client in clients_in_waiting:
+    #         if client != request.sid:
+    #             other_player = client
+    #             break
+    #
+    #     global game_number
+    #     game_number += 1
+    #     create_game(request.sid, other_player, game_number)
+    #     clients_in_waiting.remove(request.sid)
+    #     clients_in_waiting.remove(other_player)
+    #     emit('redirect', {'url': url_for('game')}, request.sid)
+    #     emit('redirect', {'url': url_for('game')}, other_player)
 
 
 if __name__ == "__main__":

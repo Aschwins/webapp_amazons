2ba79ef79debcb3ec984585dc545ae745e2a5bc6
Aschwins <aschwin.schilperoort@gmail.com>
2020-02-07 09:38:48 +0100

    added games to db model

--- amazons/models.py
+++ amazons/models.py
@@ -1,4 +1,5 @@
 from amazons import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -6,6 +7,15 @@ class User(db.Model):
     username = db.Column(db.String(80), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    games = db.relationship('Games', backref='player', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Games(db.Model):  # many to many with User
+    id = db.Column(db.Integer, primary_key=True)
+    white_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    black_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    result = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow )
